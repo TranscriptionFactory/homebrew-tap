4193b2509fda4d47d580890e37bf5966be8dc419
Transcription Factory <transcriptionfactory@gmail.com>
2026-03-25 08:01:18 -0400

    Add carbide cask

---
+++ Casks/carbide.rb
@@ -0,0 +1,24 @@
+cask "carbide" do
+  version "0.0.0"
+
+  on_arm do
+    sha256 "placeholder"
+    url "https://github.com/TranscriptionFactory/carbide/releases/download/v#{version}/carbide_#{version}_aarch64.dmg"
+  end
+
+  on_intel do
+    sha256 "placeholder"
+    url "https://github.com/TranscriptionFactory/carbide/releases/download/v#{version}/carbide_#{version}_x64.dmg"
+  end
+
+  name "Carbide"
+  desc "Carbide"
+  homepage "https://github.com/TranscriptionFactory/carbide"
+
+  app "carbide.app"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+end
